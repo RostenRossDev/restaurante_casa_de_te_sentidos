030d3f343b2d0309fab01a01b7c80fba9d2f5797
Elias <Joselias-Pascuet@outlook.com>
2022-09-11 17:26:58 -0300

    Agregando nuevas clases a Menu
    MenuController y sus funciones e annotations y IMenuDao

--- api/src/main/java/com/sentidos/api/controllers/MenuController.java
+++ api/src/main/java/com/sentidos/api/controllers/MenuController.java
@@ -1,4 +1,24 @@
 package com.sentidos.api.controllers;
 
+import com.sentidos.api.entities.Menu;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/menu/")
 public class MenuController {
+
+    @GetMapping
+    public Menu allMenu(){
+
+        return null;
+    }
+    @GetMapping
+    public Menu menuById(){
+
+        return null;
+    }
+
+
 }
